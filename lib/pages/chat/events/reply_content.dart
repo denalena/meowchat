a65d9352887f8af87af8703306221c6500b7802e
dena <hello@denalena.de>
2025-12-16 01:30:20 +0100

    fix spoilers!

--- lib/pages/chat/events/reply_content.dart
+++ lib/pages/chat/events/reply_content.dart
@@ -80,6 +80,7 @@ class ReplyContent extends StatelessWidget {
                     MatrixLocals(L10n.of(context)),
                     withSenderNamePrefix: false,
                     hideReply: true,
+                    plaintextBody: true,
                   ),
                   overflow: TextOverflow.ellipsis,
                   maxLines: 1,
